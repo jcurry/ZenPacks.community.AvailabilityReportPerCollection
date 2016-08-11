da0adda42320311a1e5f6912d6d9c942f66122f3
Jane Curry <jane.curry@skills-1st.co.uk>
2016-08-11 10:04:16 +0100

    fixed libexec location

--- ZenPacks/community/AvailabilityReportPerCollection/libexec/reportmail.sh
+++ ZenPacks/community/AvailabilityReportPerCollection/libexec/reportmail.sh
@@ -15,7 +15,7 @@
 #
 # The  following example emails the report from this ZenPack, hosted on the zenoss server zen42.class.example.org.
 # The Zenoss GUI user/password are jane / object00.
-# The email recipient is jane.curry@skills-1st.co.uk, the sender is zenoss@skilss-1st.co.uk, subject "test of reportmail"
+# The email recipient is jane.curry@skills-1st.co.uk, the sender is zenoss@skills-1st.co.uk, subject "test of reportmail"
 # Selection fields for the report are also included (after the ?) so:
 #               DeviceClass=%2F 
 #               System=%2F
@@ -36,5 +36,5 @@
  
 # See the screenhots directory for examples of both the Zenoss GUI and the email
 #
-reportmail -u  "https://zen42.class.example.org/zport/dmd/Reports/Performance%20Reports/Availability%20Report%20per%20Collection?DeviceClass=%2F&System=%2F&DeviceGroup=%2F&Location=%2F&device=zenn&startDate=07%2F26%2F2016&endDate=08%2F02%2F2016&eventClass=%2FStatus%2FPing&severity=4" -a jane.curry@skills-1st.co.uk  -s "test of reportmail" -f zenoss@skills-1st.co.uk  -U jane -p object00 
+reportmail -u  "https://zen42.class.example.org/zport/dmd/Reports/Performance%20Reports/Availability%20Report%20per%20Collection?DeviceClass=%2F&System=%2F&DeviceGroup=%2F&Location=%2F&device=zenn&startDate=08%2F01%2F2016&endDate=08%2F12%2F2016&eventClass=%2FStatus%2FPing&severity=4" -a jane.curry@skills-1st.co.uk  -s "test of reportmail" -f zenoss@skills-1st.co.uk  -U jane -p object00 
  
